6cab6fef9e73b2840e341b65ae2dfd58098ab9ec
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-11 23:34:20 +0900

    Added commends about RouterBinding

--- src/modules/router_binding.ts
+++ src/modules/router_binding.ts
@@ -2,6 +2,11 @@
 /** Signature for the callback function that is called when a location path updated. */
 export type RouterUpdateListener = (newPath: string) => void;
 
+/**
+ * This singleton class that manages routing and state updates.
+ * Listens to history changes and notifies registered listeners
+ * when the current path changes.
+ */
 export class RouterBinding {
     private static _instance: RouterBinding;
     private constructor() {
@@ -13,8 +18,8 @@ export class RouterBinding {
     }
 
     /** This values defines listeners that is called when a location path updated. */
-    listeners: RouterUpdateListener[] = [];
-    
+    protected listeners: RouterUpdateListener[] = [];
+
     addListener(listener: RouterUpdateListener) {
         console.assert(!this.listeners.includes(listener), "Already exists a given listener in the context.");
         this.listeners.push(listener);
@@ -25,23 +30,43 @@ export class RouterBinding {
         this.listeners = this.listeners.filter(l => l != listener);
     }
 
+    /** Notifies staticly a given path to registered listeners. */
     notifyListeners(newPath: string) {
         this.listeners.forEach(l => l(newPath))
     }
 
+    /**
+     * Navigate to a new path if it differs from the current path.
+     * Updates the history stack and notifies listeners of the new path.
+     */
     push(path: string) {
         if (location.pathname != path) {
             this.notifyListeners(path), history.pushState(null, "", path);
         }
     }
 
-    replace(path: string) {
+    /**
+     * Move to a new path by first going back in the history stack
+     * and then pushing the new path. Notifies listeners of the new path.
+     * 
+     * Therefore, This just merely replaces the current path
+     * with a given path, And the path depth does not change.
+     */
+    move(path: string) {
         if (location.pathname != path) {
-            this.notifyListeners(path), history.replaceState({}, "", path);
+            history.back();
+            history.pushState({}, "", path);
+            this.notifyListeners(location.pathname);
         }
     }
 
-    pop(path: string) {
-        this.notifyListeners(path), history.back();
+    /** Move forward in the history stack and notify listeners of the current path. */
+    forward() {
+        history.forward(), this.notifyListeners(location.pathname);
+    }
+
+    /** Move backward in the history stack and notify listeners of the current path. */
+    backward() {
+        history.back(), this.notifyListeners(location.pathname);
     }
 }
